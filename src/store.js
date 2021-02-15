49412e17acdc96ed771ac6b1f164707f1a5fb171
Slava Lugin <lughin.sviatoslav@mail.ru>
2021-02-15 19:12:03 +0300

    initialize

---
+++ src/store.js
@@ -0,0 +1,12 @@
+import { applyMiddleware, createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk";
+
+import reducers from 'reducers'
+
+const middlewares = [thunk]
+
+export default store = createStore(
+    reducers,
+    composeWithDevTools(applyMiddleware(...middlewares))
+)
